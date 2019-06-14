d6e30bce8c24e05d4ac4a7c2428ed9700ae723fc
Yannis Tarfa <info@yannistarfa.com>
2019-06-13 23:15:30 -0400

    Added basic recording functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import 'node-audiorecorder';
+import startRecording from './startRecording';
+const fs = require('fs');
+const path = require('path');
 
-console.log("Hello, World");
+// Create path to write recordings to
+const recordingsDirectory = "recordings";
+if (!fs.existsSync(recordingsDirectory)) {
+    fs.mkdirSync(recordingsDirectory);
+}
+// Create file path with random name
+const fileName = path.join(recordingsDirectory, Math.random().toString(36).replace(/[^a-z]+/g, ``).substr(0, 4).concat(`.wav`));
+console.log("Writing new recording file at", fileName, "\n");
+// Start recording and pipe to fileName
+startRecording(fileName);
+
+// Keep process alive
+process.stdin.resume();
+console.warn("Press ctrl+c to exit.");
