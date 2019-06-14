be01cdfe5a0d437186c95904cd13af10ea7a9175
Yannis Tarfa <info@yannistarfa.com>
2019-06-14 00:07:23 -0400

    Added webpack to project with base configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist/client',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist/client',
+        hot: true
+    }
+};
