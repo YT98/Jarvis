becadab884e3df5696f14c4abe694a1c674b4eda
Yannis Tarfa <info@yannistarfa.com>
2019-06-14 00:07:46 -0400

    Added missing import from client index.js

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Get root div
+let root = document.getElementById("root");
+
+// Render app into root
+ReactDOM.render(
+    <App />,
+    root
+);
