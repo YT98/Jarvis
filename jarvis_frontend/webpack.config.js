d123b2a5290fe06700327b20790d39b24c95d041
Yannis Tarfa <info@yannistarfa.com>
2019-06-22 01:03:09 -0400

    Added cors headers to webpack dev server

--- jarvis_frontend/webpack.config.js
+++ jarvis_frontend/webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
     ],
     devServer: {
         contentBase: './dist',
-        hot: true
+        hot: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        }
     }
 };
