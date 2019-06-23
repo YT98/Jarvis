11cbec1d60664274f26d959b75e88cb05d0fb019
Yannis Tarfa <info@yannistarfa.com>
2019-06-23 02:33:37 -0400

    Created home view sections

--- jarvis_frontend/src/Home.js
+++ jarvis_frontend/src/Home.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 import HomeWeatherWidget from './weatherComponents/HomeWeatherWidget';
 import HomeDateTimeWidget from './miscComponents/HomeDateTimeWidget';
+
+const HomeContainer = styled.div`
+
+`
+
+const LeftSection = styled.div`
+    float: left;
+    width: 200px;
+`
+
+const RightSection = styled.div`
+    float: right;
+    width: 200px;
+`
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
-                <HomeDateTimeWidget />
-                <HomeWeatherWidget />
-            </div>
+            <HomeContainer>
+
+                <LeftSection>
+                    <HomeDateTimeWidget />
+                    <HomeWeatherWidget />
+                </LeftSection>
+
+                <RightSection>
+
+                </RightSection>
+                
+            </HomeContainer>
         )
     }
 }
