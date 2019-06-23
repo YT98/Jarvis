ca2e9578fa59fd5854772ffd5bcbfc3e8c963d79
Yannis Tarfa <info@yannistarfa.com>
2019-06-23 02:33:45 -0400

    Added basic date time fx

--- jarvis_frontend/src/miscComponents/HomeDateTimeWidget.js
+++ jarvis_frontend/src/miscComponents/HomeDateTimeWidget.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const ClockWidget = styled.div`
+`
+
+const DateWidget = styled.div`
+`
 
 export default class HomeDateTimeWidget extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =  {}
+    }
+
+
+    componentDidMount() {
+        setInterval(() => {
+            let date = new Date();
+            this.setState({
+                time: `${date.getHours()}:${date.getMinutes()}`,
+                date: `${monthArray[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`,
+                weekday: `${weekdayArray[date.getDay()]}`
+            })
+        }, 1000)
+    }
+
     render() {
         return (
             <div>
-                <p> Date Time Widget </p>
+                <p>{this.state.time}</p>
+                <p>{this.state.weekday}</p>
+                <p>{this.state.date}</p>
             </div>
         )
     }
