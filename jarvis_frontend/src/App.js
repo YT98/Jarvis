4775e662d832799f02e67158fc8665fac5bda44d
Yannis Tarfa <info@yannistarfa.com>
2019-06-26 20:21:38 -0400

    Added global and App styling

--- jarvis_frontend/src/App.js
+++ jarvis_frontend/src/App.js
@@ -2,6 +2,24 @@ import React from 'react';
 import openSocket from 'socket.io-client';
 import Home from './Home';
 
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`
+
+const AppContainer = styled.div`
+    background-color: #7C989D;
+    height: 100vh;
+    width: 100vw;
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 100;
+    color: white;
+    padding: 50px;
+`
+
 export default class App extends React.Component {
 
     constructor(props) {
@@ -26,10 +44,11 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <p> {this.state.appName} </p>
+            <AppContainer>
+                <GlobalStyle/>
+                {/* <p> {this.state.appName} </p> */}
                 <Home/>
-            </div>
+            </AppContainer>
         )
     }
 }
