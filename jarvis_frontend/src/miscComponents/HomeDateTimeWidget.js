1484bd9ec9ebd395d76250cbd955e3dc998f77f1
Yannis Tarfa <info@yannistarfa.com>
2019-06-26 20:22:40 -0400

    Improved and styled clock & date widget

--- jarvis_frontend/src/miscComponents/HomeDateTimeWidget.js
+++ jarvis_frontend/src/miscComponents/HomeDateTimeWidget.js
@@ -5,35 +5,65 @@ const weekdayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "F
 const monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const ClockWidget = styled.div`
+    p {
+        font-size: 70px;
+        margin: 0 0 15px 0;
+    }
 `
 
 const DateWidget = styled.div`
+    p {
+        font-size: 20px;
+        margin: 0 0 5px 0;
+    }
 `
 
 export default class HomeDateTimeWidget extends React.Component {
     constructor(props) {
         super(props);
         this.state =  {}
+        this.setDate = this.setDate.bind(this);
     }
 
+    setDate() {
+        let date = new Date();
+        let hours = date.getHours() < 10 ? `0${date.getHours()}` : `${date.getHours()}`;
+        let minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
+        let day = date.getDate();
+        switch(day) {
+            case (1):
+                day = "1st";
+                break;
+            case (2):
+                day = "2nd";
+                break;
+            case (3):
+                day = "3rd";
+                break;
+            default:
+                day = day.toString() + "th";
+        }
+        this.setState({
+            time: `${hours}:${minutes}`,
+            date: `${monthArray[date.getMonth()]} ${day}`,
+            weekday: `${weekdayArray[date.getDay()]}`
+        })
+    }
 
     componentDidMount() {
-        setInterval(() => {
-            let date = new Date();
-            this.setState({
-                time: `${date.getHours()}:${date.getMinutes()}`,
-                date: `${monthArray[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`,
-                weekday: `${weekdayArray[date.getDay()]}`
-            })
-        }, 1000)
+        setInterval(() => { this.setDate() }, 1000);
     }
 
     render() {
         return (
             <div>
-                <p>{this.state.time}</p>
-                <p>{this.state.weekday}</p>
-                <p>{this.state.date}</p>
+                <ClockWidget>
+                    <p>{this.state.time}</p>
+                </ClockWidget>
+                <DateWidget>
+                    <p>{this.state.weekday}</p>
+                    <p>{this.state.date}</p>
+                </DateWidget>
             </div>
         )
     }
