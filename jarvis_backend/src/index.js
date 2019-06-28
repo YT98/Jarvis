750dc393eb76e656bd4abae6338ca638e44e4784
Yannis Tarfa <info@yannistarfa.com>
2019-06-27 20:13:11 -0400

    Changed index.js to reflect folder and file changes

--- jarvis_backend/src/index.js
+++ jarvis_backend/src/index.js
@@ -1,81 +1,34 @@
-import express from 'express';
-const app = express();
 import cors from 'cors';
-import cron from 'node-cron';
+import weatherRouter from './weatherApi/weatherRouter';
+import express from 'express';
 import { initializeSocket } from './socket';
-import { getCurrentWeather, saveCurrentWeather } from './getWeather';
-import { getHeadlines } from './getNews';
-const SOCKET_PORT = 8000;
-const APP_PORT = 5000;
+import { startRecording } from './audio/startRecording';
+import { actionSwitch } from './mirrorActions';
+import { getTranscription } from './speechApi/speechApi';
+const app = express();
 
 // Express middleware configuration
 app.use(cors());
+app.use('/weather', weatherRouter);
 
 // Socket initialization
 initializeSocket(SOCKET_PORT, app);
 
-// Get current weather with cron job
-cron.schedule('0 * * * *', () => {
-    saveCurrentWeather();
-    console.log("Saving current weather - runs every hour.");
-});
-
-// App specific routes for development purposes
-// To be later replaced by speech recognition fx
-const calendarAppJson = {
-    "app": "calendar",
-    "options": {
-        "scale": "day",
-        "date": "01/01/2019",
-        "startDate": "null",
-        "endDate": "null"
-    }
-}
-
-app.get('/weather', (req, res) => {
-    getCurrentWeather()
-    .then(weatherData => {
-        let socketData = {
-            "app": "weather",
-            "data": weatherData
-        }
-        req.io.emit('message', JSON.stringify(socketData));
-        res.send("Data emitted.");
-    });
-});
-
-app.get('/currentweather', (req, res) => {
-    let data = require('./currentWeather.json');
-    res.header("Content-Type", "application/json");
-    res.send(JSON.stringify(data));
-});
-
-app.get('/news', (req, res) => {
-    getHeadlines();
-});
-
-import { startRecording } from './audio/startRecording';
-app.get('/record', (req, res) => {
-    startRecording();
-    res.send("Recording started");
-});
-
-import { actionSwitch } from './mirrorActions';
-import { getTranscription } from './speechApi';
+// Speech recognition
 app.get('/transcribe', async (req, res) => {
     startRecording()
-    .then(fileName => {
-        getTranscription(fileName)
-        .then(transcription => {
-            let action = actionSwitch(transcription);
-            res.send(action);
+        .then(fileName => {
+            getTranscription(fileName)
+                .then(transcription => {
+                    let action = actionSwitch(transcription);
+                    res.send(action);
+                })
+                .catch(e => console.log(e));
         })
         .catch(e => console.log(e));
-    })
-    .catch(e => console.log(e));
 });
 
-
-// Socket server listening
 // Express server listening
+const SOCKET_PORT = 8000;
+const APP_PORT = 5000;
 app.listen(APP_PORT, () => console.log(`Express server listening on port ${APP_PORT}`));
