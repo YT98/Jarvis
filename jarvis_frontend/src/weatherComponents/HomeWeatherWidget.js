ebe72af7295c33a40fad753978a1fe99b44ab1bd
Yannis Tarfa <info@yannistarfa.com>
2019-06-28 16:59:58 -0400

    Added weather description to HomeWeatherWidget

--- jarvis_frontend/src/weatherComponents/HomeWeatherWidget.js
+++ jarvis_frontend/src/weatherComponents/HomeWeatherWidget.js
@@ -10,15 +10,22 @@ function importAllIcons(r) {
 const weatherIcons = importAllIcons(require.context('../images/weatherIcons', false, /\.svg$/));
 
 const HomeWeatherWidgetContainer = styled.div`
-    display: flex;
-    align-items: center;
-    p {
-        display: inline-block;
-        font-size: 80px;
-        margin: 0 10px 0 0;
+    .temperature-icon {
+        display: flex;
+        align-items: center;
+        .temperature {
+            display: inline-block;
+            font-size: 80px;
+            margin: 0 10px 0 0;
+        }
+        .icon {
+            height: 80px;
+        }
     }
-    img {
-        height: 80px;
+    .weather-description {
+        display: block;
+        text-align: right;
+        font-size: 25px;
     }
 `
 
@@ -46,8 +53,11 @@ export default class HomeWeatherWidget extends React.Component {
     render() {
         return (
             <HomeWeatherWidgetContainer>
-                <p> {this.state.currentTemp}&#176; </p>
-                <img src={weatherIcons[`${this.state.weather}.svg`]} />
+                <div class="temperature-icon">
+                    <p class="temperature"> {this.state.currentTemp}&#176; </p>
+                    <img class="icon" src={weatherIcons[`${this.state.weather}.svg`]} />
+                </div>
+                <p class="weather-description"> {this.state.description} </p>
             </HomeWeatherWidgetContainer>
         )
     }
