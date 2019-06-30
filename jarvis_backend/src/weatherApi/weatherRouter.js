8bb09d700e4693e5d6d01f9ed332e5f8390dab9e
Yannis Tarfa <info@yannistarfa.com>
2019-06-30 14:42:36 -0400

    Added getWeatherFromFile to /weather/current

--- jarvis_backend/src/weatherApi/weatherRouter.js
+++ jarvis_backend/src/weatherApi/weatherRouter.js
@@ -1,27 +1,11 @@
 import express from 'express';
-import fs from 'fs';
-import { getCurrentWeather, saveCurrentWeather } from './getWeather';
+import { getWeatherFromFile, saveCurrentWeather } from './getWeather';
 
 const router = express.Router();
 
-// router.get('/save', (req, res) => {
-//     getCurrentWeather()
-//         .then(weatherData => {
-//             let socketData = {
-//                 "app": "weather",
-//                 "data": weatherData
-//             }
-//             req.io.emit('message', JSON.stringify(socketData));
-//             res.send("Data emitted.");
-//         });
-// });
-
 router.get('/current', (req, res) => {
-    fs.readFile(__dirname + '/../currentWeather.json', (e, data) => {
-        if (e) console.log(e);
-        res.header("Content-Type", "application/json");
-        res.send(data);
-    });
+    res.header('content-type', 'application/json');
+    res.send(getWeatherFromFile());
 });
 
 router.get('/current/save', (req, res) => {
