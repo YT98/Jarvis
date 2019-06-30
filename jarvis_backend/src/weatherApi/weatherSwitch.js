d0f012234c3bca0e9cb5ccd82b298581d799579f
Yannis Tarfa <info@yannistarfa.com>
2019-06-30 14:43:08 -0400

    Added data handling to "weather today" in weatherSwitch

--- jarvis_backend/src/weatherApi/weatherSwitch.js
+++ jarvis_backend/src/weatherApi/weatherSwitch.js
@@ -1,9 +1,27 @@
 import { testTranscription } from '../mirrorActions';
+import { getWeatherFromFile } from './getWeather';
+import fs from 'fs';
+
+function weatherData() {
+    let weatherData;
+    fs.readFile(__dirname + '/../currentWeather.json', (e, data) => {
+        if (e) console.log(e);
+        weatherData = data;
+    });
+    return weatherData;
+}
 
 function weatherSwitch(transcription) {
+    let weatherData = getWeatherFromFile();
     switch(true) {
         case(testTranscription(/today/, transcription)):
-            return "weather today";
+            return {
+                appName: "weather",
+                appData: {
+                    type: "daily",
+                    dataPoints: weatherData.daily
+                }
+            };
             break;
         case(testTranscription(/next week/, transcription)):
             return "weather next week";
